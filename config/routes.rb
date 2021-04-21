5672fe5e9636a6204cd361f83469f147b90bc432
rikako-yurino <dawnpurple.1989@gmail.com>
2021-04-21 17:00:40 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'smoothies/index'
-  get 'users/index'
   devise_for :users
   root to: 'smoothies#index'
+  resources :users, only: [:show]
+  resources :smoothies, only: [:index, :new]
 end
--- db/migrate/20210421060316_create_foodstuffs.rb
+++ db/migrate/20210421060316_create_foodstuffs.rb
@@ -3,7 +3,7 @@ class CreateFoodstuffs < ActiveRecord::Migration[6.0]
     create_table :foodstuffs do |t|
       t.string         :foodstuff,       null: false
       t.string         :quantity
-      t.references     :smoothie_id,     null: false, foreign_key: true
+      t.references     :smoothie,        null: false, foreign_key: true
       t.timestamps
     end
   end
